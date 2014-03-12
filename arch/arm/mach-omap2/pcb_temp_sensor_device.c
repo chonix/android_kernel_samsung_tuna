7c4ffedbfe5b8fc6ad2d28a65122d868926084d1
Chonix <jhonnyx@gmail.com>
2014-03-12 15:37:58 -0300

    Revert

--- arch/arm/mach-omap2/pcb_temp_sensor_device.c
+++ arch/arm/mach-omap2/pcb_temp_sensor_device.c
@@ -1,8 +1,7 @@
 /*
- * OMAP on die Temperature sensor device file
+ * PCB Temperature sensor device file
  *
  * Copyright (C) 2011 Texas Instruments Incorporated - http://www.ti.com/
- * Author: J Keerthy <j-keerthy@ti.com>
  *
  * This program is free software; you can redistribute it and/or
  * modify it under the terms of the GNU General Public License
@@ -26,19 +25,17 @@
 #include <plat/omap_device.h>
 #include "control.h"
 #include "pm.h"
-#include <plat/temperature_sensor.h>
+#include <plat/pcb_temperature_sensor.h>
 
-void omap_temp_sensor_resume_idle(void)
+void pcb_temp_sensor_resume_idle(void)
 {
-	omap_temp_sensor_idle(0);
 }
 
-void omap_temp_sensor_prepare_idle(void)
+void pcb_temp_sensor_prepare_idle(void)
 {
-	omap_temp_sensor_idle(1);
 }
 
-static struct omap_device_pm_latency omap_temp_sensor_latency[] = {
+static struct omap_device_pm_latency pcb_temp_sensor_latency[] = {
 	{
 	 .deactivate_func = omap_device_idle_hwmods,
 	 .activate_func = omap_device_enable_hwmods,
@@ -46,15 +43,15 @@ static struct omap_device_pm_latency omap_temp_sensor_latency[] = {
 	}
 };
 
-static int temp_sensor_dev_init(struct omap_hwmod *oh, void *user)
+static int pcb_temp_sensor_dev_init(struct omap_hwmod *oh, void *user)
 {
-	struct omap_temp_sensor_pdata *temp_sensor_pdata;
+	struct pcb_temp_sensor_pdata *temp_sensor_pdata;
 	struct omap_device *od;
 	static int i;
 	int ret = 0;
 
 	temp_sensor_pdata =
-	    kzalloc(sizeof(struct omap_temp_sensor_pdata), GFP_KERNEL);
+	    kzalloc(sizeof(struct pcb_temp_sensor_pdata), GFP_KERNEL);
 	if (!temp_sensor_pdata) {
 		pr_err
 		    ("%s: Unable to allocate memory for %s.Error!\n",
@@ -62,14 +59,13 @@ static int temp_sensor_dev_init(struct omap_hwmod *oh, void *user)
 		return -ENOMEM;
 	}
 
-	temp_sensor_pdata->offset = OMAP4_CTRL_MODULE_CORE_TEMP_SENSOR;
+	temp_sensor_pdata->name = "pcb_temp_sensor";
 
-	temp_sensor_pdata->name = "omap_temp_sensor";
-
-	od = omap_device_build(temp_sensor_pdata->name, i, oh, temp_sensor_pdata,
+	od = omap_device_build(temp_sensor_pdata->name, i, oh,
+			       temp_sensor_pdata,
 			       sizeof(*temp_sensor_pdata),
-			       omap_temp_sensor_latency,
-			       ARRAY_SIZE(omap_temp_sensor_latency), 0);
+			       pcb_temp_sensor_latency,
+			       ARRAY_SIZE(pcb_temp_sensor_latency), 0);
 	if (IS_ERR(od)) {
 		pr_warning("%s: Could not build omap_device for %s: %s.\n\n",
 			   __func__, temp_sensor_pdata->name, oh->name);
@@ -83,13 +79,10 @@ done:
 	return ret;
 }
 
-int __init omap_devinit_temp_sensor(void)
+int __init pcb_devinit_temp_sensor(void)
 {
-	if (!cpu_is_omap446x() && !cpu_is_omap447x())
-		return 0;
-
 	return omap_hwmod_for_each_by_class("thermal_sensor",
-			temp_sensor_dev_init, NULL);
+			pcb_temp_sensor_dev_init, NULL);
 }
 
-arch_initcall(omap_devinit_temp_sensor);
+arch_initcall(pcb_devinit_temp_sensor);
