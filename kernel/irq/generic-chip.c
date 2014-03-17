fc3be996971c921674c5f57e09f611c1d4b6b27c
Chonix <jhonnyx@gmail.com>
2014-03-17 15:11:45 -0300

    Revert "Updated kernel/irq/generic-chip.c android function as others."
    This reverts commit 88bf539e98d4ddeb1e85c46cbed079436ab25102.

--- kernel/irq/generic-chip.c
+++ kernel/irq/generic-chip.c
@@ -140,30 +140,6 @@ void irq_gc_mask_disable_reg_and_ack(struct irq_data *d)
 	irq_gc_lock(gc);
 	irq_reg_writel(mask, gc->reg_base + cur_regs(d)->mask);
 	irq_reg_writel(mask, gc->reg_base + cur_regs(d)->ack);
-
-	struct irq_chip_type *ct = irq_data_get_chip_type(d);
-	u32 mask = d->mask;
-
-	irq_gc_lock(gc);
-	irq_reg_writel(mask, gc->reg_base + ct->regs.mask);
-	irq_reg_writel(mask, gc->reg_base + ct->regs.ack);
-	irq_gc_unlock(gc);
-}
-
-/**
- * irq_gc_mask_and_ack_set- Mask and ack pending interrupt
- * @d: irq_data
- */
-void irq_gc_mask_and_ack_set(struct irq_data *d)
-{
-	struct irq_chip_generic *gc = irq_data_get_irq_chip_data(d);
-	struct irq_chip_type *ct = irq_data_get_chip_type(d);
-	u32 mask = d->mask;
-
-	irq_gc_lock(gc);
-	*ct->mask_cache |= mask;
-	irq_reg_writel(*ct->mask_cache, gc->reg_base + ct->regs.mask);
-	irq_reg_writel(mask, gc->reg_base + ct->regs.ack);
 	irq_gc_unlock(gc);
 }
 
