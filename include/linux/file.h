13b457120a9fbf993ea04c6833afb4714e23ae51
Chonix <jhonnyx@gmail.com>
2014-06-11 19:53:39 -0300

    BINDER: Backport binder from omapzoom android - 3.8y branch.

--- include/linux/file.h
+++ include/linux/file.h
@@ -9,6 +9,8 @@
 #include <linux/types.h>
 #include <linux/posix_types.h>
 
+#include <linux/fdtable.h>
+
 struct file;
 
 extern void fput(struct file *);
@@ -27,6 +29,17 @@ static inline void fput_light(struct file *file, int fput_needed)
 		fput(file);
 }
 
+struct fd {
+	struct file *file;
+	int need_put;
+};
+
+static inline void fdput(struct fd fd)
+{
+	if (fd.need_put)
+		fput(fd.file);
+}
+
 extern struct file *fget(unsigned int fd);
 extern struct file *fget_light(unsigned int fd, int *fput_needed);
 extern struct file *fget_raw(unsigned int fd);
@@ -34,10 +47,20 @@ extern struct file *fget_raw_light(unsigned int fd, int *fput_needed);
 extern void set_close_on_exec(unsigned int fd, int flag);
 extern void put_filp(struct file *);
 extern int alloc_fd(unsigned start, unsigned flags);
+extern int __alloc_fd(struct files_struct *files, unsigned start, unsigned end, unsigned flags);
 extern int get_unused_fd(void);
 #define get_unused_fd_flags(flags) alloc_fd(0, (flags))
 extern void put_unused_fd(unsigned int fd);
 
 extern void fd_install(unsigned int fd, struct file *file);
+extern void  __fd_install(struct files_struct *files, unsigned int fd, struct file *file);
+extern int __close_fd(struct files_struct *files, unsigned fd);
+
+static inline struct fd fdget(unsigned int fd)
+{
+	int b;
+	struct file *f = fget_light(fd, &b);
+	return (struct fd){f,b};
+}
 
 #endif /* __LINUX_FILE_H */
